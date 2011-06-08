c50b5acb3b8fff6ced33656a8abd3dcdcbc180f9
Fabrício Ferrari de Campos <ffc.fabricio@gmail.com>
2011-06-08 11:11:50 -0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Alfa::Application.config.secret_token = '13ecc44098b304d60fff09eca9240fb1e07b1424952f8ea69d7817d674d317854f9103dd15c3010c7a1da88c2ec6f780814e43948aa92ef7fcdd91aa6d29e03b'
