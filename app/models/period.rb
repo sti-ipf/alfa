071692d1caa4d0d3bdf790b26f5cc8ea8ebf8ae0
Fabrício Ferrari de Campos <ffc.fabricio@gmail.com>
2011-12-15 20:26:13 -0200

    arrumada constante DAYS_OF_WEEK

--- app/models/period.rb
+++ app/models/period.rb
@@ -3,7 +3,7 @@ class Period < ActiveRecord::Base
   belongs_to :core
 
   DAYS_OF_WEEK = [[I18n.t('period.monday'), 1], [I18n.t('period.tuesday'), 2],
-    [I18n.t('period.wednesday'), 3, [I18n.t('period.thursday'), 4], [I18n.t('period.friday'), 5]]
+    [I18n.t('period.wednesday'), 3], [I18n.t('period.thursday'), 4], [I18n.t('period.friday'), 5]]
 
 end
 
