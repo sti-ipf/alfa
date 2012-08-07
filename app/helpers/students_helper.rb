7a2fecf7ac8188d0cd75c2e9084ce0e93bc52935
Fabrício Ferrari de Campos <ffc.fabricio@gmail.com>
2012-08-07 20:47:30 -0300

    adicionada exportacao de csv dos monitores

--- app/helpers/students_helper.rb
+++ app/helpers/students_helper.rb
@@ -2,7 +2,7 @@ module StudentsHelper
 
   def show_students_without_mother_or_all_students(type)
     if type == 1
-      "<div class = 'presence_lists'> #{link_to 'Visualizar alfabetizandos sem o nome da mãe', without_mother_name_path, :remote => true} </div>"
+      "<div class = 'presence_lists'> #{link_to 'Sem o nome da mãe', without_mother_name_path, :remote => true} </div>"
     else
       "<div class = 'presence_lists'> #{link_to 'Visualizar todos alfabetizandos', students_path} </div>"
     end
--- app/models/educator.rb
+++ app/models/educator.rb
@@ -35,6 +35,10 @@ class Educator < ActiveRecord::Base
     return value
   end
 
+  def where_use_pc_to_s
+    get_attribute_value(self.where_use_pc, WHERE_USE_PC)
+  end
+
   def total_seats_to_s
     get_attribute_value(self.total_seats, Coordinator::TOTAL_SEATS)
   end
@@ -83,8 +87,24 @@ class Educator < ActiveRecord::Base
     end
   end
 
+  def has_pc_to_s
+    if self.has_pc
+      "Sim"
+    else
+      "Não"
+    end
+  end
+
   def know_social_network_to_s
     if self.know_social_network
+      "Sim"
+    else
+      "Não"
+    end
+  end
+
+  def travel_disponibility_to_s
+    if self.travel_disponibility
       "Sim"
     else
       "Não"
@@ -123,16 +143,101 @@ class Educator < ActiveRecord::Base
     get_attribute_value(self.internet_access_local, Student::INTERNET_ACCESS_LOCAL)   
   end
 
+  def familiar_income_to_s
+    get_attribute_value(self.familiar_income, Coordinator::FAMILIAR_INCOME)   
+  end
+
+  def age_range_to_s
+    get_attribute_value(self.age_range, AGE_RANGE)   
+  end
+
+  def special_need_type_to_s
+    get_attribute_value(self.special_need_type, SPECIAL_NEED)   
+  end
+
   def self.to_csv_file(educators)
     array = []
     educators.each do |r|
-      attributes = r.attributes.keys
-      new_array = Array.new(attributes.count)
-      i = 0
-      attributes.each do |a|
-        new_array[i] = eval("r.#{a}")
-        i += 1
-      end
+      new_array = Array.new
+      puts r.name
+      new_array << r.id  
+      new_array << r.name
+      new_array << r.start_date_to_students.try(:strftime, "%d/%m/%Y")
+      new_array << r.age
+      new_array << r.gender_to_s
+      new_array << r.dob.try(:strftime, "%d/%m/%Y") 
+      new_array << r.ethnicity_to_s
+      new_array << r.hometown
+      new_array << r.district  
+      new_array << r.number  
+      new_array << r.zip_code
+      new_array << r.state 
+      new_array << r.zone_to_s
+      new_array << r.zone_desc 
+      new_array << r.address 
+      new_array << r.civil_status_to_s
+      new_array << r.rg  
+      new_array << r.cpf 
+      new_array << r.voter_id  
+      new_array << r.professional_id 
+      new_array << r.professional_serie  
+      new_array << r.birth_or_marriage_certificate_id  
+      new_array << r.social_id 
+      new_array << r.sons_number 
+      new_array << r.first_son_age 
+      new_array << r.second_son_age  
+      new_array << r.third_son_age 
+      new_array << r.forth_son_age 
+      new_array << r.education_level_to_s
+      new_array << r.education_level_desc  
+      new_array << r.familiar_income_to_s 
+      new_array << r.house_to_s
+      new_array << r.house_desc  
+      new_array << r.house_type_to_s  
+      new_array << r.special_need_to_s
+      new_array << r.political_affiliation_to_s
+      new_array << r.political_party 
+      new_array << r.political_desc  
+      new_array << r.religion_to_s
+      new_array << r.religion_desc 
+      new_array << r.association_to_s
+      new_array << r.association_desc  
+      new_array << r.cooperative_to_s
+      new_array << r.core.try(:name)
+      new_array << r.core_identification 
+      new_array << r.age_range_to_s
+      new_array << r.internet_access_local_to_s
+      new_array << r.internet_access_local_desc  
+      new_array << r.email 
+      new_array << r.house_type_desc 
+      new_array << r.special_need_desc 
+      new_array << r.special_need_type_to_s
+      new_array << r.participation_reason  
+      new_array << r.travel_disponibility_to_s
+      new_array << r.last_books  
+      new_array << r.tv_programs 
+      new_array << r.experience  
+      new_array << r.other_job 
+      new_array << r.agree_with_any_period 
+      new_array << r.what_teach  
+      new_array << r.subjects  
+      new_array << r.has_pc_to_s
+      new_array << r.where_use_pc_to_s
+      new_array << r.about_pc_use_to_s  
+      new_array << r.pc_uses.collect(&:description).join(', ')
+      new_array << r.where_use_pc_desc 
+      new_array << r.has_internet_to_s
+      new_array << r.where_has_internet_to_s  
+      new_array << r.where_has_internet_desc 
+      new_array << r.internet_uses.collect(&:description).join(', ')
+      new_array << r.for_what_use_internet_desc  
+      new_array << r.use_social_net_works.collect(&:description).join(', ')
+      new_array << r.know_social_net_works.collect(&:description).join(', ')
+      new_array << r.know_social_network_desc  
+      new_array << r.use_social_network_desc 
+      new_array << r.observation 
+      new_array << r.identification_on.try(:strftime, "%d/%m/%Y")
+      new_array << r.profession
       array << new_array
     end
     array
